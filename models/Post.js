4a77526cc6c114c6881959ccd8e0a7be1c565413
halil <svtr.hozcan@gmail.com>
2023-03-15 10:18:52 +0100

    PostSchema

--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,26 @@
-const mongoose = require('mongoose')
-
+const mongoose = require("mongoose");
 
 const PostSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:[true, 'Please provide title'],
-        trim:true,
-        maxLength:[100, 'Title can not be more than 100 characters']
-    }
-})
+  title: {
+    type: String,
+    required: [true, "Please provide title"],
+    trim: true,
+    maxLength: [100, "Title can not be more than 100 characters"],
+  },
+  category: {
+    type: String,
+    required: [true, "Please provide category"],
+    trim: true,
+    maxLength: [100, "Category can not be more than 100 characters"],
+    enum: ["ART", "IT", "HISTORY", "FOOTBALL"],
+  },
+  image: {
+    type: String,
+    default: "/uploads/example.jpeg",
+  },
+  content: {
+    type: String,
+    required: [true, "Please provide product description"],
+    maxlength: [3000, "Content can not be more than 3000 characters"],
+  },
+});
